8c7cab7f33ef96a64a682d2f355c47079e53d664
allenmopom <allen.mopom@gmail.com>
2015-05-23 17:08:57 +0400

    First Commit

--- src/main/java/exampleMain.java
+++ src/main/java/exampleMain.java
@@ -24,12 +24,22 @@ public class exampleMain {
             price = p;
             timeStamp = new Date(t);
         }
-        public double getPrice() {return price;}
-        public String getSymbol() {return symbol;}
-        public Date getTimeStamp() {return timeStamp;}
+
+        public double getPrice() {
+            return price;
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
+
+        public Date getTimeStamp() {
+            return timeStamp;
+        }
 
         @Override
         public String toString() {
+
             return "Price: " + price.toString() + " time: " + timeStamp.toString();
         }
     }
